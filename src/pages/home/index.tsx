949971cc25b3b5985068f9e06f368580318d0cf1
Deibson Cogo <deibsoncogo@outlook.com>
2020-07-14 23:15:26 -0300

    9.2 Estilizando Dashboard (Home)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Titulo } from './styles';
+import { Titulo, CampoPesquisa, Repositorio } from './styles';
+import ImagemGithub from '../../assets/imagem-github.svg';
 
 // SEMPRE USAR PRIMEIRA LETRA MAIUSCULA PARA HOOK
 // AMBOS COMANDOS SAO IGUAIS MAIS O SEGUNDO POSSUI UM VINCULO DE TIPAGEM MAIS FACIL
 // function Home() {}
 const Home: React.FC = () => {
-	return <Titulo>Explore repositórios no Github</Titulo>;
+	return (
+		<>
+			{/* UTILIZACAO DA IMAGEM IMPORTADA */}
+			<img src={ImagemGithub} alt="Github Explorer" />
+			<Titulo>Explore repositórios no Github</Titulo>
+
+			<CampoPesquisa>
+				{/* CRIA UM CAMPO PARA PESQUISA COM ESTE TEXTO DENTRO */}
+				<input placeholder="Digite o nome do repositorio" />
+				{/* CRIA UM BOTAO DO TIPO SUBMIT COM ESTE TEXTO DENTRO */}
+				<button type="submit">Pesquisar</button>
+			</CampoPesquisa>
+
+			<Repositorio>
+				<a href="teste">
+					<img
+						src="https://avatars0.githubusercontent.com/u/62838219?s=460&v=4"
+						alt="Deibson Cogo"
+					/>
+					<div>
+						<strong>rocketseat/unform</strong>
+						<p>Descricao do repositorio</p>
+					</div>
+				</a>
+			</Repositorio>
+		</>
+	);
 };
 
 export default Home;
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,7 +1,101 @@
 import Styled from 'styled-components';
+import { shade } from 'polished';
 
 // TEMPLATE LITERALS E A UTILIZACAO DE ``
 export const Titulo = Styled.h1`
-	font-size: 48px; /*TAMANHO*/
-	color: #3a3a3a; /*COR*/
+	font-size: 48px; /* TAMANHO */
+	color: #3a3a3a; /* COR */
+	max-width: 450px; /* TAMANHO MAXIMO */
+	line-height: 56px; /* TAMANHO DO ESPACO ENTRE LINHAS */
+	margin-top: 80px; /* MARGEM SUPERIOR */
+`;
+
+export const CampoPesquisa = Styled.form`
+	margin-top: 40px;
+	max-width: 700px;
+	display: flex; /* TARFORMA ELE EM UM FLEX */
+
+	/* FAZ QUE TODO input DENTRO DO CampoPesquisa VAI SER ASSIM */
+	input {
+		flex: 1; /* FAZ O BOTAO OCULPAR TODO ESPACO DISPONIVEL DA TELA */
+		height: 70px; /* ALTURA */
+		padding: 0 24px;
+		border: 0;
+		border-radius: 5px 0 0 5px; /* BORDA INTERNA */
+		color: #3a3a3a;
+
+		&::placeholder {
+			color: #a8a8b3;
+		}
+	}
+
+	button {
+		width: 210px; /* TAMANHO */
+		height: 70px;
+		background: #04d361;
+		border-radius: 0px 5px 5px 0px;
+		border: 0px;
+		color: #ffffff;
+		font-weight: bold; /* DEIXA O TEXTO NEGRITO */
+		transition: background-color 0.2s; /* TEMPO DE ATRAZO DA ANIMACAO */
+
+		/* CRIA UM REGRA PARA EXECUTAR QUANDO TIVER UM hover */
+		&:hover {
+			background: ${shade(0.2, '#04d361')}
+		}
+	}
+`;
+
+export const Repositorio = Styled.div`
+	margin-top: 80px;
+	max-width: 700px;
+
+	a {
+		background: #ffffff;
+		border-radius: 5px;
+		width: 100%;
+		padding: 24px;
+		display: block; /* O QUE DEVE FICAR POR CIMA DO FUNDO */
+		text-decoration: none; /* REMOVE OS ERROS DOS TEXTOS */
+
+		display: flex; /* JOGA OS ITENS PARA O LADO */
+    align-items: center; /* ALINHA OS ITENS AO CENTRO */
+    transition: transform 0.2s;
+
+		/* DEFINE QUE SE O ITEM ANTERIOR DA LISTA FOR a UTILIZAR AS COFIGURACOES */
+		& + a {
+			margin-top: 16px;
+		}
+
+		&:hover {
+			transform: translatex(10px); /* ANIMACAO DE IR PARA O LADO */
+		}
+
+		img {
+			width: 64px;
+			height: 64px;
+			border-radius: 50%; /* 50% DEIXA A IMAGEM CIRCULAR */
+		}
+
+		div {
+			margin-left: 16px;
+
+			strong {
+				font-size: 20px;
+				color: #3d3d4d;
+			}
+
+			p {
+				font-size: 18px;
+				color: #a8a8b3;
+				margin-top: 4px;
+			}
+
+			/* TODO ICONE POSSUI ESTE FORMATO */
+			svg {
+				margin-left: auto; /* FAZ SE ALINHAR PARA A DIREITA */
+				color: #cbcbd6;
+			}
+		}
+	}
 `;
